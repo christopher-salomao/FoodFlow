030ae9fba8e97dd3cad8754bb61b1c64e40e44b1
devsalomao <christophersalomao5@gmail.com>
2026-06-10 13:42:31 -0300

    feat: sitema de login feito com autenticacao via JWT

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,28 +1,48 @@
 import prismaClient from "../../prisma/index";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthUserProps {
   email: string;
   password: string;
 }
 
- class AuthUserService {
+class AuthUserService {
   async execute({ email, password }: AuthUserProps) {
     console.log(email, password);
-    return "LOGADO";
-    /* const user = await prismaClient.user.findUnique({
+
+    // verificação de email
+    const user = await prismaClient.user.findFirst({
       where: {
         email,
       },
     });
 
-    if (!user) throw new Error("Usuário não encontrado");
+    if (!user) throw new Error("Dados incorretos, verifique suas credenciais");
 
+    // verificação de senha
     const passwordMatch = await compare(password, user.password);
 
-    if (!passwordMatch) throw new Error("Senha incorreta");
+    if (!passwordMatch)
+      throw new Error("Dados incorretos, verifique suas credenciais");
 
-    return user; */
+    // gerar token JWT
+    const token = sign({
+      name: user.name,
+      email: user.email,
+    }, process.env.JWT_SECRET as string, {
+      subject: user.id,
+      expiresIn: "30d"
+    })
+
+    // retornar user e token
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token
+    };
   }
 }
 
